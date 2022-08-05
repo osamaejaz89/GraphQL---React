1c2b4eb4f0a5ca666186d6d3f4d44c7de021d795
osamaejaz89 <osamaejaz89@gmail.com>
2022-08-05 13:00:37 +0500

    types & data

--- app.js
+++ app.js
@@ -44,12 +44,13 @@ app.use(
       createEvent: (args) => {
         const event = {
           _id: Math.random().toString(),
-          title: args.title,
-          description: args.description,
-          price: +args.price,
-          date: new Date().toISOString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date,
         };
         events.push(event);
+        return event;
       },
     },
     graphiql: true,
